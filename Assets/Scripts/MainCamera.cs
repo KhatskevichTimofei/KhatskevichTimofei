9fd69f01f1671027293e0ef01e22cefc3a172be4
timofey20122012@gmail.com <timofey20122012@gmail.com>
2019-12-04 15:09:16 +0700

    Возможность перемещать камеру на стрелочки

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speedCamera;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0, speedCamera) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= new Vector3(0, 0, speedCamera) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(speedCamera, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speedCamera, 0, 0) * Time.deltaTime;
+        }
+
     }
 }
