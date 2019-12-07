e1f1831fe81de3b01280c9dd600d869c1fd6a77d
timofey20122012@gmail.com <timofey20122012@gmail.com>
2019-12-07 17:49:39 +0700

    Добавлено: Новые иконки; Новый вид интерфейса; Система заданий

---
+++ Assets/Scripts/QuestObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour
+{
+    public QuestList questList;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Unit>() != null)
+        {
+            questList.QuestComplete();
+            Destroy(gameObject);
+        }
+
+    }
+}
