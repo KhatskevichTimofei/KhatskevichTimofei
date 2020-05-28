098d6e7f44d0b1123a7e5d7bf9f9f9ffb43ea87d
KhatskevichTimofei <KhatskevichTimofei@gmail.com>
2020-05-28 10:30:33 +0700

    Плавное изменение угла в скролле

--- Assets/Scripts/Simple/MainCamera.cs
+++ Assets/Scripts/Simple/MainCamera.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class MainCamera : UnityEngine.MonoBehaviour
 {
-    public float speedCamera, speedCameraOnScroll;
-    public float strangeScroll;
+    public float speedCamera, speedCameraOnScroll, strangeScroll, smoothnessOfTheThreshold, borderUp, borderDown;
     public Vector3 cameraVector;
     void Start()
     {
@@ -15,29 +14,39 @@ public class MainCamera : UnityEngine.MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow)) //Стрелочка вверх
         {
             float y = transform.position.y;
             transform.position += transform.forward * speedCamera * Time.deltaTime;
             transform.position = new Vector3(transform.position.x, y, transform.position.z);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow)) //Стрелочка вниз
         {
             float y = transform.position.y;
             transform.position -= transform.forward * speedCamera * Time.deltaTime;
             transform.position = new Vector3(transform.position.x, y, transform.position.z);
-            
+
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow)) //Стрелочка влево 
         {
             transform.position -= transform.right * speedCamera * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow)) //Стрелочка вправо
         {
             transform.position += transform.right * speedCamera * Time.deltaTime;
         }
-        transform.position -= new Vector3(0, Input.mouseScrollDelta.y, 0) * strangeScroll;
-
+        if (transform.position.y < borderUp && borderDown > transform.position.y)
+        {
+            transform.position -= new Vector3(0, Input.mouseScrollDelta.y, 0) * strangeScroll;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && transform.position.y > 272 && transform.position.y < 632)
+        {
+            transform.Rotate(-Time.deltaTime * smoothnessOfTheThreshold, 0, 0);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0 && transform.position.y > 272 && transform.position.y < 632)
+        {
+            transform.Rotate(Time.deltaTime * smoothnessOfTheThreshold, 0, 0);
+        }
         if (Input.GetMouseButtonDown(2))
         {
             cameraVector = Input.mousePosition;
